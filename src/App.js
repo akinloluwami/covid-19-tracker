44a9b88f43144a57334be109b754365b783fc8ad
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-02-23 13:08:45 +0000

    refactored the code to show worldwide stats on page load

--- src/App.js
+++ src/App.js
@@ -30,7 +30,13 @@ function App() {
     getCountriesData();
   }, []);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all").then((response) =>
+      response.json().then((data) => {
+        setCountryInfo(data);
+      })
+    );
+  }, []);
 
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
