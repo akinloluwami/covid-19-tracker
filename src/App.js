93d983915d32af09804f574712125ab8369c91f1
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-02-23 13:03:48 +0000

    fetched countries COVID stats from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import { FormControl, Select, MenuItem } from "@material-ui/core";
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import InfoBox from "./COMPONENTS/InfoBox";
+import Map from "./COMPONENTS/Map";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -22,35 +30,70 @@ function App() {
     getCountriesData();
   }, []);
 
+  useEffect(() => {}, []);
+
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
 
     setCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
   };
   return (
     <div className="app">
-      <div className="app__stats">
-        <InfoBox title="Active Cases" cases={"199"} total={"3426"} />
-        <InfoBox title="Recovered" cases={"909"} total={"4536"} />
-        <InfoBox title="Deaths" cases={"099"} total={"26"} />
+      <div className="app__left">
+        <div className="app__stats">
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+
+        <div className="app__header">
+          <h1>COVID 19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <Map />
       </div>
-
-      <div className="app__header">
-        <h1>COVID 19 TRACKER</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country} onChange={onCountryChange}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </div>
-
-      {/**Table*/}
-      {/**Graph */}
-
-      {/**Map */}
+      <Card className="app__right">
+        <CardContent>
+          {/**Table*/}
+          <h3>Live Cases by Country</h3>
+          {/**Graph */}
+          <h3>Wordwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/COMPONENTS/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Map() {
+  return (
+    <div className="map">
+      <h1>Map shit</h1>
+    </div>
+  );
+}
+
+export default Map;
