15ac2934fab5bc05b0dc126101f0591c23d7ae4c
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-02-23 09:46:49 +0000

    added countries drop-down using useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+
 function App() {
+  const [countries, setCountries] = useState(["Nigeria", "Ghana", "Canada"]);
   return (
-    <div>
-      <h1>COVID TRACKER</h1>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/**Title and Dropdown */}
+
+      {/**Box */}
+      {/**Box */}
+      {/**Box */}
+
+      {/**Table*/}
+      {/**Graph */}
+
+      {/**Map */}
     </div>
   );
 }
