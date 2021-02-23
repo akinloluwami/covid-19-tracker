8641bf15ea96c5f9b3432862c70474712d503e5d
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-02-23 10:09:56 +0000

    fetched all countries name into the drop-down menu

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import "./App.css";
 import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
-  const [countries, setCountries] = useState(["Nigeria", "Ghana", "Canada"]);
+  const [countries, setCountries] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
       <div className="app__header">
@@ -13,7 +26,7 @@ function App() {
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
             {countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
