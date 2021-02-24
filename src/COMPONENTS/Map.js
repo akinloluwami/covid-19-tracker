caf1c2fbe299a82463aae9688bef96fbe9860058
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-02-24 16:31:21 +0000

    added map

--- src/COMPONENTS/Map.js
+++ src/COMPONENTS/Map.js
@@ -6,7 +6,7 @@ function Map({ center, zoom }) {
       <MapContainer center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; Shoutout to <a href="http://osm.org/copyright">OpenStreetMap</a>'
         />
       </MapContainer>
     </div>
